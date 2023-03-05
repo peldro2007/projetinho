986940706c3fe5d05b1df97e09d6555ae1dd89e6
peldro2007 <127048875+peldro2007@users.noreply.github.com>
2023-03-05 17:39:01 -0300

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,12 @@
+function rastreiar (){
+
+    var search = document.getElementById('search').value;
+
+    if (search == 'AMAZON') {
+
+        alert("SEU PEDIDO FOI DESPACHADO AS 23:49 EM DESTINO A RIO DE JANEIRO")
+    }else{
+        alert('Seu codigo de rastreio esta errado')
+    }
+
+}
