e78e56dd1303f8da7eef366e81ffb79b2a66c179
peldro2007 <127048875+peldro2007@users.noreply.github.com>
2023-03-18 17:29:56 -0300

    Add files via upload

---
+++ projetinho/js/main.js
@@ -0,0 +1,99 @@
+function rastreiar (){
+
+    var search = document.getElementById('search').value;
+
+    if (search == 'MLSCYZLQX3C') { 
+
+        alert("SEU PEDIDO FOI DESPACHADO AS 23:49 EM DESTINO A RIO DE JANEIRO - RJ")}
+    
+    else if (search == 'MLS4F27D53V') {
+         
+        alert('SEU PEDIDO FOI DESPACHADO AS 23:49 EM DESTINO A SAO PAULO - SP')}
+
+    else if (search == 'MLSYKZ8PSN4') {
+         
+            alert('SEU PEDIDO FOI DESPACHADO AS 23:49 EM DESTINO A SANTA CATARINA - SC')}
+
+     else if (search == 'MLSHQVF84Y3') {
+         
+                alert('SEU PEDIDO FOI DESPACHADO AS 23:49 EM DESTINO A MINAS GERAS - MG')}
+    
+    else if (search == 'MLSZJD28BTA') {
+         
+                    alert('SEU PEDIDO FOI DESPACHADO AS 23:49 EM DESTINO A ESPIRITO SANTO - ES')}
+
+    else if (search == 'MLSSADF87Y') {
+         
+                        alert('SEU PEDIDO FOI DESPACHADO AS 23:49 EM DESTINO A RIO GRANDE DO SUL - RS')}
+    
+    else if (search == 'MLSFWCVB7T6') {
+         
+                            alert('SEU PEDIDO FOI DESPACHADO AS 23:49 EM DESTINO A PARANA - PR')}
+    else if (search == 'MLSSDF78YASD') {
+         
+                                alert('SEU PEDIDO FOI DESPACHADO AS 23:49 EM DESTINO A ACRE - AC')}
+else if (search == 'MLSAFSYB71') {
+         
+                                    alert('SEU PEDIDO FOI DESPACHADO AS 23:49 EM DESTINO A ALAGOAS AL')}
+else if (search == 'MLSAFSW6G7') {
+         
+                                        alert('SEU PEDIDO FOI DESPACHADO AS 23:49 EM DESTINO A AMAPA - AP')}
+else if (search == 'MLSASFE76G') {
+         
+                                            alert('SEU PEDIDO FOI DESPACHADO AS 23:49 EM DESTINO A AMAZONAS - AM')}
+else if (search == 'MLSASDF76S') {
+         
+                                                alert('SEU PEDIDO FOI DESPACHADO AS 23:49 EM DESTINO A BAHIA - BA')}
+
+else if (search == 'MLSASEF56T7') {
+         
+                                                    alert('SEU PEDIDO FOI DESPACHADO AS 23:49 EM DESTINO A CEARA - CE')}
+else if (search == 'MLSSA67DFGG') {
+         
+                                                        alert('SEU PEDIDO FOI DESPACHADO AS 23:49 EM DESTINO A DISTRITO FEDERAL - DF')}
+else if (search == 'MLSESGR76T') {
+         
+                                                            alert('SEU PEDIDO FOI DESPACHADO AS 23:49 EM DESTINO A GOIAS - GO')}
+else if (search == 'MLSSDAF67Y') {
+         
+                                                                alert('SEU PEDIDO FOI DESPACHADO AS 23:49 EM DESTINO A MARANHAO - MA')}
+else if (search == 'MLSG43Q67Y80') {
+         
+                                                                    alert('SEU PEDIDO FOI DESPACHADO AS 23:49 EM DESTINO A MATO GROSSO - MT')}
+else if (search == 'MLS78HGBFDRS') {
+         
+                                                                        alert('SEU PEDIDO FOI DESPACHADO AS 23:49 EM DESTINO A MATO GROSSO DO SUL - MS')}
+else if (search == 'MLSSEG8UJ9') {
+         
+                                                                            alert('SEU PEDIDO FOI DESPACHADO AS 23:49 EM DESTINO A PARA - PA')}
+else if (search == 'MLS2F37H8G') {
+         
+                                                                                alert('SEU PEDIDO FOI DESPACHADO AS 23:49 EM DESTINO A PERNAMBUCO - PE')}
+ else if (search == 'MLSSGDEA67H8') {
+         
+                                                                                    alert('SEU PEDIDO FOI DESPACHADO AS 23:49 EM DESTINO A PIAUI - PI')}
+else if (search == 'MLSFGDS78') {
+         
+                                                                                        alert('SEU PEDIDO FOI DESPACHADO AS 23:49 EM DESTINO A RIO GRANDE DO NORTE - RN')}
+else if (search == 'MLSDSG78YH') {
+         
+                                                                                            alert('SEU PEDIDO FOI DESPACHADO AS 23:49 EM DESTINO A RONDONIA - RO')}
+else if (search == 'MLSSADF87') {
+         
+                                                                                                alert('SEU PEDIDO FOI DESPACHADO AS 23:49 EM DESTINO A RORAIMA = RR')}
+else if (search == 'MLSSDAFNH') {
+         
+                                                                                                    alert('SEU PEDIDO FOI DESPACHADO AS 23:49 EM DESTINO A SERGIPE - SE')}      
+else if (search == 'MLSASDF87H1') {
+         
+                                                                                                        alert('SEU PEDIDO FOI DESPACHADO AS 23:49 EM DESTINO A TOCANTINS - TO')}                                                                                
+                                                                                    
+    else {
+
+        alert('Seu Codigo de Rastreio esta errado')
+    }
+    
+    
+    
+    
+    }
